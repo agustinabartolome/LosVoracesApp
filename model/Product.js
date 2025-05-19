a53bbe38bfcc34b2f786afab85f78f15d1047bb4
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2025-05-18 23:05:17 -0300

    Adding files: model and controller

---
+++ model/Product.js
@@ -0,0 +1,15 @@
+class Product{
+
+    constructor(productId, name, category, isbn, price, author, section, stock, literaryGenre){
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.author = author;
+        this.isbn = isbn;
+        this.section = section;
+        this.price = price;
+        this.stock = stock;
+        this.literaryGenre = literaryGenre;
+
+    }
+}
