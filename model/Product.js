f0a9e8f76e983759324f4fe0199b72e6ddb5c2d5
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2025-05-19 20:10:26 -0300

    update file

--- model/Product.js
+++ model/Product.js
@@ -1,15 +1,8 @@
 class Product{
 
-    constructor(productId, name, category, isbn, price, author, section, stock, literaryGenre){
+    constructor(productId, name, category){
         this.productId = productId;
         this.name = name;
         this.category = category;
-        this.author = author;
-        this.isbn = isbn;
-        this.section = section;
-        this.price = price;
-        this.stock = stock;
-        this.literaryGenre = literaryGenre;
-
     }
 }
