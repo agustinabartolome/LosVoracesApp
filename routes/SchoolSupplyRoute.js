812a17385a701af5ce1538d12e318d3d1f16fd17
Rosinaer <rosinaer@hotmail.com>
2025-05-22 18:20:25 -0300

    create routes folder and SchoolSupplyRoutes.js file

---
+++ routes/SchoolSupplyRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const schoolSupplyController = require('../controller/SchoolSupplyController');
+
+router.get('/', schoolSupplyController.getSchoolSupply);
+router.post('/', schoolSupplyController.createSchoolSupply);
+router.put('/:id', schoolSupplyController.updateSchoolSupply);
+router.delete('/:id', schoolSupplyController.deleteSchoolSupply);
+
+module.exports = router;
