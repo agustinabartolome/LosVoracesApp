7a958560408210bf69d3f2da12b48be759cc4023
Rosinaer <rosinaer@hotmail.com>
2025-05-22 18:51:26 -0300

    connect SchoolSupply routes in app.js

--- App.js
+++ App.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const SchoolSupplyRoute = require('./routes/SchoolSupplyRoute');
+
+// Middleware
+app.use(express.json()); 
+
+// RUTAS 
+app.use('/schoolSupplies', SchoolSupplyRoute);
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
