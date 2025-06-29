8cd2bec96ae619ccf8a60e6f12071ceb1e69506d
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2025-06-29 18:56:26 -0300

    actuazlización de rutas para registro y login

--- Version-1.1/routes/DashboardRoute.js
+++ Version-1.1/routes/DashboardRoute.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateToken } = require('../middleware/AuthMiddleware');
+const { authenticateToken, authorizeRole } = require('../middleware/AuthMiddleware');
 
-router.get('/dashboard', authenticateToken, (req, res) => {
-  
+router.get('/dashboard', authenticateToken, authorizeRole('owner', 'manager'), (req, res) => {
   res.render('dashboard', { user: req.user });
 });
 
