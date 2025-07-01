ff0caa830d45f899013bfe18bb3ea79a445b6c61
AbrilBartolome <107815760+AbrilBartolome@users.noreply.github.com>
2025-06-30 21:55:38 -0300

    Update config.js

--- Version-1.1/frontend/js/config.js
+++ Version-1.1/frontend/js/config.js
@@ -2,5 +2,5 @@ const isLocalhost = window.location.hostname === 'localhost';
 
 const BACKEND_URL = isLocalhost
   ? 'http://localhost:3000'
-  : 'https://.onrender.com';
+  : 'https://los-voraces-app-backend.onrender.com/';
 
