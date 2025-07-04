427b650da1061b772721eec422da18eb5840760b
AbrilBartolome <107815760+AbrilBartolome@users.noreply.github.com>
2025-07-03 21:42:17 -0300

    Create logout.js

---
+++ Version-1.1/frontend/js/logout.js
@@ -0,0 +1,18 @@
+document.getElementById('logoutBtn').addEventListener('click', async () => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/auth/logout`, {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    if (res.ok) {
+      alert('Sesión cerrada correctamente');
+      window.location.href = 'login.html';
+    } else {
+      alert('Error al cerrar sesión');
+    }
+  } catch (err) {
+    console.error('Error cerrando sesión:', err);
+    alert('No se pudo cerrar la sesión');
+  }
+});
