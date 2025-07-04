6c9a7f27d5ba793ad683f932b7ed3f314f03714f
AbrilBartolome <107815760+AbrilBartolome@users.noreply.github.com>
2025-07-03 22:06:39 -0300

    Update logout.js

--- Version-1.1/frontend/js/logout.js
+++ Version-1.1/frontend/js/logout.js
@@ -1,18 +1,25 @@
-document.getElementById('logoutBtn').addEventListener('click', async () => {
-  try {
-    const res = await fetch(`${BACKEND_URL}/auth/logout`, {
-      method: 'GET',
-      credentials: 'include',
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const logoutBtn = document.getElementById('logoutBtn');
 
-    if (res.ok) {
-      alert('Sesión cerrada correctamente');
-      window.location.href = 'login.html';
-    } else {
-      alert('Error al cerrar sesión');
-    }
-  } catch (err) {
-    console.error('Error cerrando sesión:', err);
-    alert('No se pudo cerrar la sesión');
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', async () => {
+      try {
+        const res = await fetch(`${BACKEND_URL}/auth/logout`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (res.ok) {
+          alert('Sesión cerrada correctamente');
+          window.location.href = 'login.html';
+        } else {
+          alert('Error al cerrar sesión');
+        }
+      } catch (err) {
+        console.error('Error cerrando sesión:', err);
+        alert('No se pudo cerrar la sesión');
+      }
+    });
   }
 });
+
