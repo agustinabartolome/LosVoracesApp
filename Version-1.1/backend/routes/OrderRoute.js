f59faefc98479750331dceb4ba454c1d2d5299fb
AbrilBartolome <107815760+AbrilBartolome@users.noreply.github.com>
2025-07-04 00:23:19 -0300

    Update OrderRoute.js

--- Version-1.1/backend/routes/OrderRoute.js
+++ Version-1.1/backend/routes/OrderRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const OrderController = require('../controller/OrderController');
 const { authenticateToken, authorizeRole } = require('../middleware/AuthMiddleware');
 
+router.get('/catalog', authenticateToken, OrderController.renderOrders);
+
 router.get('/', authenticateToken, authorizeRole('owner'), OrderController.getOrders);
 router.post('/', authenticateToken, authorizeRole('owner'), OrderController.createOrder);
 router.put('/:id', authenticateToken, authorizeRole('owner'), OrderController.updateOrder);
